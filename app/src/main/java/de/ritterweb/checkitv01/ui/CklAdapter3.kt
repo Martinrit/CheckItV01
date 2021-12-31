6703792dc65a822d28219028952389325b20cfd8
Martinrit <martin@ritterweb.de>
2021-12-31 19:21:14 +0100

    Das Fragement und die Recyclerview läuft, jetzt müssen noch die OnClick Funktionen im Adapter und im HomeFragement eingerichtet werden

---
+++ app/src/main/java/de/ritterweb/checkitv01/ui/CklAdapter3.kt
@@ -0,0 +1,119 @@
+package de.ritterweb.checkitv01.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import de.ritterweb.checkitv01.R
+import de.ritterweb.checkitv01.repository.database.Ckl
+import kotlinx.android.synthetic.main.item_rv_main.view.*
+
+/////////////////////////////////////////////////////////////////////////////
+//  Die Adapterklasse stellt einen Adapter bereit, mit dem Daten in die itemViews der Zeilen einer Recyclerview eingefüllt werden
+//  und der auf bestimmte Ereignisse reagiert.
+//
+//  Die Reaktion auf Click Events wird hier ebenfalls vorbereitet, allerdings werden hier nur Interfaces bereitgestellt, die im Fragment oder der Activitiy überschrieben werden
+//  ( override). Damit ist der Adapter universell nutzbar, die Logik, z.B. die Reaktion auf Klicks auf ein View Item wird die die aufrufende UI Klasse verlagert
+//
+//  Aufruf
+//        private val cklLists: List<Ckl>    : hier wird die Datenquelle und deren Typ übergeben
+//        Der Typ ist RecyclerView.Adapter als ckladatper3.ExampleViewHolder, also der hier in dieser Classe definerten ExampelViewHolder
+//
+class CklAdapter3(private var cklLists: ArrayList<Ckl>):RecyclerView.Adapter<CklAdapter3.ExampleViewHolder> (){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExampleViewHolder {
+        /////////////////////////////////////////////////////////////////////////////
+        //
+        // OnCreateViewHolder lägt für jede Zeile in der Recyclerview eine View an.
+        //
+        // 'itemView' ist eine interne Variable, die hierzu angelegt wird. Sie wird zum Schluss per return zurückgegeben
+        // Die View itemView wird über den LayoutInflate angelegt:
+        // Syntax ist immer gleich, dennoch hier kurz zur Erläuterung:
+        //      .from(parent.context)  : Bezug auf den Kontext des Parents. Parent ist die Recyclerview, deren Context ist das Fragment in dem sie stehckt
+        //      .inflate (....)   Das XML File, in dem die Viewelemente definiert sind.
+        //      .parent   Das ist die Recyclerview in der die View als Zeile angelegt wird.
+        //      false ; attachToRoot  gibt an, das die View nicht direkt angelegt wird
+        //  Diese Syntax ist grundsätzlich immer gleich: val itemView = LayoutInflater.from(parent.context).inflate((R.layout.xxxxxxx),parent,false))
+        //  Lediglich die xml Source ist spezifisch
+        //
+        //  Erläuerung der Aufrufparameter: onCreateViewHolder(parent: ViewGroup, viewType: Int)
+        //      parent: das ist die Recyclerview in der der Viewholder aufgerufen wird
+        //      viewType:  das wird nur vewendet, wenn innerhalb der RecyclerView unterschiedliche ItemViews aufgerufen werden sollen
+        //
+        //  onCreateViewHolder wird nur ein paar mal aufgerufen. Nämlich für die itemviews, die in de RecyclerView angezeigt werden
+        //  ( also zum Beispiel 10 Stück, je nach Größe) sowie ein paar drüber unter drunter für das flüsseige Scrollen
+        //  entsprechend wenig Rechenleistung wird dafür benötit
+        // im Gegensatz dazu  wird onBindViewHolder für alle Datensätze angelegt die angezeigt werden.
+        // Das bedeutet, das die Methode bei jedem Scrollen für jedes neu angezeigte Element aufgerufen wird.
+        //
+        // Daher stammt auch der Name RecyclerView. Eine itemView, die durch Scrollen verschwindet wird auf de anderen Seite wieder
+        // "recycelt" alsi mit anderen Daten befüllt und erneut angezeigt
+        //
+        ///////////////////////////////////////////////////////////////////////////////
+        val itemView = LayoutInflater.from(parent.context).inflate((R.layout.item_rv_main),parent,false)
+
+        return ExampleViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ExampleViewHolder, position: Int) {
+
+        /////////////////////////////////////////////////////////////////////////////
+        //   OnBindViewHolder füllt den Holder ( das ist die ItemView) mit den Date
+        //   das passiert beim ersten Aufbau und beim Scrollen für jeden Eintrag, Also viel Rechenleistung
+        //
+        //   Aufrufparameter:onBindViewHolder(holder: ExampleViewHolder, position: Int)
+        //      holder: das ist der konkrete ViewHolder der gefüllt werden soll   ( hier der in der eigenen Sub-Klasse definerte ExampleViewHolder
+        //      position: das ist die Position im Dataset der genutzt werden soll
+        //
+        //
+        /////////////////////////////////////////////////////////////////////////////
+
+        val currentItem = cklLists[position]   // currentItem, ist der Eintrag aus dem Dataset, der angezeigt werdensoll ( übergabeparameter position)
+        holder.tvName.text = currentItem.name
+        holder.tvBeschreibung.text = currentItem.beschreibung
+        holder.ivIcon.setImageResource(currentItem.status)     // die einfacher zu verwendende Methode 'ivIcon.imageRessource' gibt es nicht, also muss mit dem Setter "ivIcon.setImageRessource()" gearbeitet werden
+
+        /// hier wird mit den im ViewHolder angelegten vals gearbeitet   ( "gecasht"), da ansonsten bei jedem Aufruf für einen Datensatz beimScrollen die
+        /// FindbyId Funktion direkt oder indirekt auferufen werden müsste, was sehr langsam ist.
+
+    }
+
+    override fun getItemCount() = cklLists.size
+    //   Das ist als Funktion ein Einzeiler und nochmal  einfacher zu schreiben als
+    //      override fun getItemCount(): Int{
+    //          return ckllist.size
+    //      }
+    // macht aber genau das gleiche
+
+
+
+
+
+    class ExampleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        //  ViewHolder  gibt es soviele wie gerade auf dem Screen angezeigt werden.
+        //  Sie werden beim Aufbau des REcyclerViews angelegt.
+        //  Alles was hier gemacht wird, ist für den jeweiligen View Holder ( Das sind nur einige)
+        //  die TextViews des itemViews als variablen einmalig anzulegen und ihnen die IDs der
+        //  der TextViews aus dem item.View Layoutfile zuzuordnen
+        //
+        // Die Definition der TextViews etc. als 'val'  führt zu einer statischen Zuordnung
+        // Sie können danach keinem anderen textView mehr zugewiesen werden,
+        // allerdings können die Werte des Textviews-Objektes später geändert werden
+        // Dies geschiet jedesmall wenn in der der itemViews beimScrollen neue Datensätze
+        // in einen der ViewHolder geladen werden.
+
+        val tvName: TextView = itemView.tvName
+        val tvBeschreibung: TextView = itemView.tvBeschreibung
+        val ivIcon: ImageView = itemView.ivIcon
+
+
+    }
+
+    fun updateContent(cklListstoupdate:ArrayList<Ckl>)
+    {
+        cklLists = cklListstoupdate
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/de/ritterweb/checkitv01/ui/HomeFragment.kt
+++ app/src/main/java/de/ritterweb/checkitv01/ui/HomeFragment.kt
@@ -5,12 +5,9 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import de.ritterweb.checkitv01.R
@@ -21,46 +18,85 @@ import de.ritterweb.checkitv01.ui.main.MainViewModelFactory
 import kotlinx.android.synthetic.main.fragment_home.view.*
 
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment(R.layout.fragment_home){
+    //  Class des Fragments:
+    // Die Klasse wird aus Fragment abgeleitet, übergeben wird dabei das xml-Layout des Fragments
 
+    // Anlegen der Variablen und Objekte die in der ganzen Klasse genutzt werden
+    //
+    // rootview:  die View der übergeordneten View
     private lateinit var rootView: View
 
-    // RecyclerView:
+    // Variablem fr die RecyclerView:
+    //  1. Objekt für die Recyclerview
+    //  2. der Adapter der Recyclerview
+    //  alle Variablen sind als Lateinit definiert, dass bedeutet, dass sie später von Funktionen innerhalb der Klasse initialisiert werden
     private lateinit var rv: RecyclerView
-    private lateinit var adapter: CklAdapter
+    private lateinit var adapter: CklAdapter3
 
-    // MainViewModel:
+    // MainViewModel:  Hier die Verbindung zum MainViewModell der Anwendung, inder in drei Stufen
+    //     1. Stufe : das MainViewModel  Zugriff auf AppRepository und Verabeitungslogik für die Variablen ( nicht in UI! definieren, damit UI unabhängig von Datenlogik und Berechnungen bleibt)
+    //     2. Stufe : das App Repository - Wird vom Main View Model aufgerufen und greift auf die unterschiedlichen Datenquellen zu, u.a. Datenbank
+    //     3. Stufe : die DAO der Datenbank
     private lateinit var mainViewModel: MainViewModel
 
+    //////////////////////////////////////////////////////////////////
+    // Vorbereitung ViewBinding im Fragement
+    // _binding:  Das ist notwendig und entspricht dem Standard beim ViewBinding in einem Fragment. Die Variable bleibt bestehen solang das Fragment besteht.
+    // erklärung unter  https://developer.android.com/topic/libraries/view-binding
+    // die FragmentHomeBinding für das Segment wird automatisch generiert wenn dies im gradle File so gesetzt ist.
+    // Besonderheit beim ViewBinding in Fragments: Da die Fragments auch nach dem Schließen im Hintergrund bestehen bleiben
+    // muss die angelegte Instance der xxxBinding Klasse stets beim Schließen des Fragments gelöscht werden
+    //
+    //Erklärung: zum !! Operator Hiner Binding:
+    //    Das Problem ist, dass _binding vom Typ FragementHomeBinding definiert und auf null gesetzt wurde
+    //    Da direkt die Binding Variable binding advon abgeleitet wird. Damit das nicht zu einer Fehlermeldung führt wird hier  der !! Operator eingesetzt
+    // !! operator  Erklärung in https://kotlinlang.org/docs/null-safety.html#the-operator
+    //
     private var _binding: FragmentHomeBinding? = null
-
-
-    // This property is only valid between onCreateView and
-// onDestroyView.
     private val binding get() = _binding!!
 
-    companion object {
-        fun newInstance() = HomeFragment()
-    }
+// Companion Object - Wurde in einem der Tutorials gemacht, Ich lasse das erstmal weg
+    // Die Logik, ist, dass zunächst eine Klasse angelegt wird und dann innerhalb der Instand der Klasse gearbeitet wird
+    // Ich versuche es mal ohne.
+    // Wenn man das wieder einschaltet, werden alle Klassen innerhalb des Companion Objects definiert
+    //
+//    companion object {
+//        /////////////////////////////////////////////////////////
+//        // Anlegen eines Companion Objects:
+//        // Was das ist, erklärt https://kotlinlang.org/docs/object-declarations.html#object-declarations-overview
+//        // Habe ich nocht nicht ganz verstanden.
+//
+//        fun newInstance() = HomeFragment()
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ):View {
+        super.onCreate(savedInstanceState)
+
+        // _binding wird dem FragementxxxxBinding des Fragements gleichgesetzt
+        // damit ist dann auch binding gefüllt, dass ja in
+        //  der Klasse ganz oben  per
+        //              private val binding get() = _binding!!
+        // dem _binding entspricht.
+        // damit ist auch binding gefüllt und aktiv
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
         val view = binding.root
 
-        return view
-    }
 
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        binding.rvHome.adapter = CklAdapter3(ArrayList())
 
-        rootView = view
+        /////////
+        rootView = binding.root
 
-        initRecyclerView()
+        adapter = CklAdapter3(ArrayList())
+
+        binding.rvHome.adapter = adapter
 
 
         mainViewModel = ViewModelProvider(
@@ -70,45 +106,62 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
 
 
-
-
-
+        ///////////////////////////////////////////
+        // Observer einrichten
+        // Es wird ein Observer auf die LiveCkls im MainViewModel gesetzt
+        // immer wenn ein Eintrag geändert wurde wird für alle items das ausgeführt was in den geschweiften Klammern steht)
         mainViewModel.getLiveCkls().observe(viewLifecycleOwner, Observer { items ->
             adapter.updateContent(ArrayList(items))
-       })
-
-        binding.buttonLogin.setOnClickListener {
-            val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
-            findNavController().navigate(action)
-        }
-
-
+        })
+        return view
     }
 
-    private fun initRecyclerView() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        rootView = view
+
         rv = rootView.findViewById(R.id.rv_home)
-        adapter = CklAdapter(ArrayList())
+
+        adapter =  CklAdapter3(ArrayList())
         rv.adapter = adapter
 
-        // Implement the Interfaces:
-        adapter.setOnItemClickListener(object:CklAdapter.OnItemClickListener{
-            override fun setOnItemClickListener(pos: Int) {
-                Log.v("asdf","Click")
-            }
-
+        mainViewModel = ViewModelProvider(
+            requireActivity(),
+            MainViewModelFactory(requireActivity().application)
+        ).get(MainViewModel::class.java)
+        mainViewModel.getLiveCkls().observe(viewLifecycleOwner, Observer { items ->
+            adapter.updateContent(ArrayList(items))
         })
 
-        adapter.setOnItemLongClickListener(object:CklAdapter.OnItemLongClickListener{
-            override fun setOnItemLongClickListener(pos: Int) {
-                Log.v("asdf","LongClick")
-            }
-        })
+//        // Implement the Interfaces:
+//        adapter.setOnItemClickListener(object:CklAdapter2.OnItemClickListener {
+//            override fun setOnItemClickListener(pos: Int) {
+//
+//                Log.v("asdf", "Click")
+//            }
+//        })
+//
+//            binding.buttonLogin.setOnClickListener {
+//            val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
+//            findNavController().navigate(action)
+//        }
+
 
     }
 
+
+
     override fun onDestroyView() {
+    // Besonderheit beim ViewBinding in Fragments: Da die Fragments auch nach dem Schließen im Hintergrund bestehen bleiben
+    // muss die angelegte Instance der xxxBinding Klasse stets beim Schließen des Fragments gelöscht werden
         super.onDestroyView()
         _binding = null
     }
+
+//    override fun setOnItemClickListener(position: Int) {
+//        Log.v("asdf","Click")
+//    }
 
 }
